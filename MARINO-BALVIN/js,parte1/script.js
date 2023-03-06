89292c1e2a8d2c6e64770fe866c747acb2e4420d
marino balvin falconi <jbfalconi@hotmail.com>
2023-03-06 14:14:51 +0100

    jas

--- MARINO-BALVIN/js,parte1/script.js
+++ MARINO-BALVIN/js,parte1/script.js
@@ -1,10 +1,32 @@
-var a= 1;
-let b= 2;
-const c= 3;
+/* commento 1
 
-console.log("ciao")
-console.log(a)
-console.log(b)
-console.log(c)
+commento
 
+*/
+
+// commento 2
+
+// varibili --> scatole che contengono valori
+// var e let sono variabili
+var a = 1;
+let b = 2;
+// let let = 1; --> Non è consentito usare 'let' come nome in dichiarazioni 'let' o 'const'
+// const è una costante --> è una scatola che non può essere più cambiata dopo la sua dichiarazione
+const COSTANTE = 3; 
+console.log("ciao");
+console.log(a, b, COSTANTE);
+
+a=5;
+console.log("a = "+a)
+b=3;
+a=b+1; // 3 + 1 = 4 
+console.log(a, b);
+
+let parola = "ciao mondo"
+console.log(parola)
+console.log(parola + " holaaa")
+
+let variabile = COSTANTE
+variabile = variabile + 1
+console.log("variabile = "+variabile)
 
