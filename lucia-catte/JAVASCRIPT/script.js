153c5427b8fe3768194ddbd853ca62c7de18eafc
Lucia Catte <cattelucia@gmail.com>
2023-03-06 10:45:44 +0100

    mod

--- lucia-catte/JAVASCRIPT/script.js
+++ lucia-catte/JAVASCRIPT/script.js
@@ -3,10 +3,10 @@
 
 var a = 1;
 let b = 2;
-const c = 3;
+const C = 3; // la variabile const non può essere cambiato il valore dopo la sua dichiarazione
 
 console.log("ciao");
-console.log(a, b, c);
+console.log(a, b, C);
 
 
 a=5;
@@ -15,7 +15,6 @@ a=b+1; // 3 + 1 = 4
 
 console.log(a);
 console.log(b);
-
 let parola = "ciao mondo";
 console.log(parola);
 console.log(parola + "hola");
