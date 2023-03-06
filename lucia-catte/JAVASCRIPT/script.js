abc4fb56a56d0f9572f3d062dfc59d1440b5adc9
Lucia Catte <cattelucia@gmail.com>
2023-03-06 10:37:24 +0100

    iniziamo JavaScript

---
+++ lucia-catte/JAVASCRIPT/script.js
@@ -0,0 +1,21 @@
+/* COMMENTO IN PIU RIGHE*/ 
+// COMMENTO IN UNA RIGA
+
+var a = 1;
+let b = 2;
+const c = 3;
+
+console.log("ciao");
+console.log(a, b, c);
+
+
+a=5;
+b=3;
+a=b+1; // 3 + 1 = 4
+
+console.log(a);
+console.log(b);
+
+let parola = "ciao mondo";
+console.log(parola);
+console.log(parola + "hola");
