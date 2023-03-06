75a41b7f36166314d94675fd4a2683b3411fb74b
JasminePizzino <jasminepizzino@hotmail.it>
2023-03-06 10:56:33 +0100

    aggiornamento

---
+++ jasmine-pizzino/js-pt1/script.js
@@ -0,0 +1,31 @@
+//commento su una riga
+/* commento su più righe */ 
+
+
+//var e let sono variabili
+var a = 1;
+let b = 2;
+//let let = 1; --> Non è consentito usare 'let' come nome in dichiarazione'let' o 'const'
+// const è una costante
+const c = 3;
+console.log("ciao");
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+
+
+a=5;
+b=3;
+a=b+1; // 3+1=4
+console.log(a);
+console.log(b);
+
+
+let parola = "ciao mondo"
+console.log(parola)
+
+let variabile = costante 
+variabile = variabile + 1 
+console.log("variabile = "+ variabile)
