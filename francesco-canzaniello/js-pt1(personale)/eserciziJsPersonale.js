3ae83ecef9a6b672c85c76d36802800f0bc9ea6d
Francesc Canzaniello <canzaniello.francesco1988@gmail.com>
2023-03-06 18:04:46 +0100

    fine primo giorno js

---
+++ francesco-canzaniello/js-pt1(personale)/eserciziJsPersonale.js
@@ -0,0 +1,11 @@
+
+let vettore1 = [2, 6, 9, 10, -2, -3, 0, 2, 5, 1];
+n = 0;
+sommVettore = 0;
+for (let index = 0; index < vettore1.length; index++) {
+    console.log(vettore1[index]);
+    sommVettore+= vettore1[index];
+    
+}
+console.log(sommVettore)
+
