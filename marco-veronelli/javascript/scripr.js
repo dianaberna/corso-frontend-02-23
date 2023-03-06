2dee83d455c0fce950e1db2c9beb0ee7036d0041
Marco Veronelli <mveronelli954@gmail.com>
2023-03-06 18:02:50 +0100

    javascript lesson

---
+++ marco-veronelli/javascript/scripr.js
@@ -0,0 +1,51 @@
+/* let l = [ 2, 6, 9, 10, -2, -3, 0, 2, 5, 1];
+cont=0;
+
+// 1
+for(cont=0; cont<=l.length; cont=cont++){
+console.log(l[cont]);
+
+}; */
+let serie = [ 2, 6, 9, 10, -2, -3, 0, 2, 5, 1];
+/* console.log(serie);
+let i=0;
+
+while (i<serie.length) {
+
+    console.log(serie[i]);
+    i++;
+} */
+
+
+
+/* let i=0
+let sommaSerie=0;
+while (i < serie.length) { 
+    sommaSerie= sommaSerie + serie[i];
+    i++;
+}
+console.log(sommaSerie); */
+
+
+/* let numeriDispari=0
+let i=0;
+let sommaSerie=0;
+while (i < serie.length) { 
+    console.log(serie[i])
+    sommaSerie= sommaSerie + serie[i];
+    i=i+2;
+}
+console.log(sommaSerie); */
+
+/* let i=0;
+let sommaSerie=0;
+while (i < serie.length) { 
+    console.log(serie[i])  
+    if (serie[i] % 2 == 1) {
+        sommaSerie=sommaSerie + serie[i]
+        console.log(serie[i])
+    }
+    
+    i=i+1;
+}
+console.log(sommaSerie); */
