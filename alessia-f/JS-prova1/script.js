93f350579d39bb3355d3623cdb193a9a626f1d71
“Alessia <alessiafalanga_94@hotmail.it>
2023-03-06 22:49:20 +0100

    responsive desktop & mobile

---
+++ alessia-f/JS-prova1/script.js
@@ -0,0 +1,22 @@
+
+
+var a = 1;
+let b= 2;
+
+const costante = 3; 
+console.log ("ciao")
+console.log(a)
+console.log(b)
+console.log (c)
+
+
+a=5;
+console.log ("a="+a)
+b=3;
+a=b+1; //3 + 1 = 4//
+
+let parola ="ciao mondo"
+console.log(parola)
+console.log(parola + "holaa")
+
+costante =5;
