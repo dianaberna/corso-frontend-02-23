433bbee8c5e76d5c1fc75c7f931a485ac1b765a0
Elisa Boscani <elisabosc90@gmail.com>
2023-03-06 10:56:43 +0100

    javascript

---
+++ elisa-boscani/modulo1/teoria/javascript/script.js
@@ -0,0 +1,20 @@
+var a = 1;
+let b = 2;
+
+const COSTANTE = 3;
+console.log("ciao");
+console.log(a, b, COSTANTE);
+
+a = 5;
+b = 3;
+a = b + 1;
+console.log(a);
+console.log(b);
+
+let parola = "ciao mondo";
+console.log(parola);
+console.log(parola + " holaa ");
+
+let variabile = COSTANTE;
+variabile = variabile + 1;
+console.log("variabile =" + variabile);
