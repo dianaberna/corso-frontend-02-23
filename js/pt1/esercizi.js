99f62b119e25c448bd86a0c06edfd3c13ab265a5
Diana Bernabei <bernabei.d@gmail.com>
2023-03-07 10:21:44 +0100

    aggiornati commenti script.js

--- js/pt1/esercizi.js
+++ js/pt1/esercizi.js
@@ -1,10 +1,9 @@
 let array = [2, 6, 9, 10, -2, -3, 0, 2, 5, 1];
-//           0  1  2                       9  10
+//           0  1  2                    9  10
 
-console.log("esercizi");
+console.log("esercizi.js");
 
 // 1) Stampare tutti gli elementi dell’array -> scorrere tutto l'array -> while/for
-console.log("1");
 
 /*
 variabile per la condizione
@@ -14,17 +13,19 @@ while(condizione){
 }
 */
 
-console.log("con while");
+console.log("esercizio 1 - con while");
 
 // condizione falsa è quando "usciamo" dall'array
+// while/for -> strumenti che mi aiutano a scorrere da 0 a 4 
+// array[i] -> elemento dell'array chiamato array nella posizione i
 
-// strumento che mi aiuta a scorrere da 0 a 4 -> while
 let i = 0;
 while (i < array.length) {
-    console.log(array[i]); // --> [] ci permette di accedere all'elemento in posizione i dell'array numeriBari
+    console.log(array[i]); // --> [] in questo caso ci permette di accedere all'elemento in posizione i dell'array numeriBari
     i = i + 1;
 }
 
+console.log("esercizio 1 - con for");
 for (let i = 0; i < array.length; i++) {
     console.log(array[i]);
 }
@@ -41,30 +42,17 @@ while (i <= lunghezza - 1) {
     //console.log("array[" + i + "] = " + array[i], "somma = " + somma);
     i++;
 }
-console.log("somma = " + somma);
-
-/*
-    i=0 arraySum=0
-    0 <= 9 ? si
-        arraySum = 0 + 2 = 2
-        i=0+1=1
-    1 <= 9 ? si
-        arraySum = 2 + 6 = 8
-    
-        ...
-    10 <= 9 ? no
-
-*/
+console.log("esercizio 2 = " + somma);
 
 // 3) Stampare la somma dei soli elementi positivi → 35
-// array[i] -> elemento dell'array chiamato array nella posizione i
+
 let sommaPositivi = 0;
 for (let i = 0; i < array.length; i++) {
     if (array[i] >= 0) {
         sommaPositivi = sommaPositivi + array[i];
     }
 }
-console.log("sommaPositivi = " + sommaPositivi);
+console.log("esercizio 3 = " + sommaPositivi);
 
 // let array = [2, 6, 9, 10, -2, -3, 0, 2, 5, 1];
 // 3.1) contare quanti elementi positivi ci sono nell'array
