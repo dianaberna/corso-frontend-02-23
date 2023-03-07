0628acfb3ca9c217a59c8ab802ca2a59413fa007
Diana Bernabei <bernabei.d@gmail.com>
2023-03-07 14:49:24 +0100

    aggiunti es fino al 9

--- js/pt1/esercizi.js
+++ js/pt1/esercizi.js
@@ -54,19 +54,6 @@ for (let i = 0; i < array.length; i++) {
 }
 console.log("esercizio 3 = " + sommaPositivi);
 
-// let array = [2, 6, 9, 10, -2, -3, 0, 2, 5, 1];
-// 3.1) contare quanti elementi positivi ci sono nell'array
-
-i = 0;
-let variabileContatore = 0;
-while (i < array.length) {
-    if (array[i] >= 0) {
-        variabileContatore = variabileContatore + 1;
-    }
-    i++;
-}
-console.log("esercizio 3.1 = " + variabileContatore);
-
 // 4) Stampare la somma dei soli elementi dispari → 12
 i = 0;
 somma = 0;
@@ -87,7 +74,68 @@ while (i < array.length) {
     i++;
 }
 // 6) Stampare quante volte compare il numero 2 → 2
+i=0;
+let cont=0;
+while(i<array.length){
+    if(array[i] == 2){
+        cont++;
+    }
+    i++;
+}
+console.log("esercizio 6, cont = "+cont)
+
+// 6.1) contare quanti 2 e -2 ci sono nell'array -> conto tutto insieme
+i=0;
+cont=0;
+while(i<array.length){
+    if(array[i] == 2 || array[i] == -2){ // and && -> array[i] == 2 e array[i] == -2 -> mai vero
+        cont++;
+    }
+    i++;
+}
+
+// 6.2) contare quanti 2 e -2 ci sono nell'array -> conto in due variabili separate
+i=0;
+let cont1=0;
+let cont2=0;
+while(i<array.length){
+    if(array[i] == 2 ){
+        cont1++;
+    }
+    if(array[i] == -2){
+        cont2++;
+    }
+    i++;
+}
+console.log("ci sono "+cont1+" numeri 2 e ci sono "+cont2+" numeri -2")
+
 // 7) Stampare quanti numeri positivi (compreso lo zero) ci sono nell’array → 8
+// quanti numeri => cont
+// positivi (compreso lo zero) =>  array[i] >= 0 -> if
+// ci sono nell’array => array -> while 
+i=0;
+cont=0;
+while(i<array.length){
+    if(array[i] >= 0){
+        cont++;
+    }
+    i++;
+}
+console.log("esercizio 7, cont = "+cont)
+cont = 0 
+i = 0; // non è necessario azzerare prima la i se andiamo ad utilizzare un for in cui mettiamo let i = 0
+// console.log("stampo i prima del for -> "+i)
+for(let i=0; i<array.length; i++){
+    // console.log("stampo i all'inizio del for -> "+i)
+    if(array[i] >= 0){
+        cont++;
+    }
+    // console.log("stampo i alla fine del for -> "+i)
+    // l'incremento viene eseguito alla fine del corpo del for
+}
+//console.log("stampo i dopo il for -> "+i)
+console.log("esercizio 7, cont = "+cont)
+
 // 8) Stampare il numero massimo (senza usare funzioni matematiche) → 10
 let max = array[0];
 i=0;
@@ -101,6 +149,7 @@ while(i < array.length){
     } 
     i++;
 }
+
 console.log("esercizio 8, massimo = "+max)
 // 9) Stampare il numero minimo → -3
 let min = array[0];
