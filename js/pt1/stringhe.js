5687ad959cdbd1f8b1800116e3d5ff1a6a870ae9
Diana Bernabei <bernabei.d@gmail.com>
2023-03-08 11:04:13 +0100

    aggiunto nuovo file teoria

---
+++ js/pt1/stringhe.js
@@ -0,0 +1,9 @@
+// stringa --> array
+// frase --> stringa separata da spazi ma sempre tra le stesse virgolette
+// gruppo di più stringhe --> array di stringhe
+
+let frase = "ciao mondo sono diana"
+console.log(frase[11])
+let parola = ["ciao", "pippo"]
+console.log(parola)
+console.log(parola[0])
