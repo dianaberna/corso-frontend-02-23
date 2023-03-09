a75a5fbdc95ba2d81b03c5d58fd401d7e173147c
Mattia Schifano <mat19189@gmail.com>
2023-03-09 11:45:14 +0100

    aggiunto es 3/4

--- mattia-schifano/js-mattia/esercizi-mattia.js
+++ mattia-schifano/js-mattia/esercizi-mattia.js
@@ -185,14 +185,90 @@ console.log(array3 + "array3");
 //  console.log(array3)
 
 //15) Creare un array3 con gli elementi (non la posizione) dispari di array1 e gli elementi pari di array2 → versione1 [1,3,4,2,2,4] 	versione2 [1,4,2,2,3,4]
-array3=[];
+array4=[];
 
 for (let i = 0; i < array1.length; i++) {
     if(array1[i] % 2 ==1){
-        array3.push(array1[i]);
+        array4.push(array1[i]);
     }
     if(array2[i] % 2 ==0){
-        array3.push(array2[i]);
+        array4.push(array2[i]);
     }
 }
-console.log(array3)
+console.log(array4)
+
+//16
+
+
+
+
+//17 Verificare se array1 e array2 dati sono palindromi come ad esempio “anna”, “radar”, “emme”, “1221”, “4224
+
+i=0;
+array1 = [ 1, 2, 2, 3, 4] ;
+array2 = [4, 2, 2, 4];
+let palindromo= true;
+palindromo="no palindromo"
+for (let i = 0; i < array1.length/2; i++) {
+    if (array1[i] !==array1[array1.length - i -1]) {
+        palindromo= "fake"
+        
+    }
+
+}
+console.log(palindromo)
+
+//true
+palindromo= true;
+for (let i = 0; i < array2.length/2; i++) {
+    if (array2[i] !== array2[array2.length - i -1]) {
+        palindromo= fake;
+        
+    }
+
+}
+console.log(palindromo)
+
+
+//1 nuovo Scrivere un programma che conta quante lettere “e” ci sono nella parola precipitevolissimevolmente 
+let parola= "precipitevolissimevolmente";
+let conteggio = 0;
+i= 0;
+for (let i = 0; i < parola.length; i++) {
+    if (parola[i] === "e") {
+        conteggio++;
+    }
+}
+
+console.log("Il numero di lettere= " + conteggio ,"e")
+
+//2 nuovo Scrivere un programma che verifica  se due stringhe sono palindrome
+
+
+
+
+
+//3  Scrivere un programma che verifica se esiste una stringa all'interno di un altra stringa 
+
+let p1 = "coraggio";
+let p2 = "raggio";
+let conparole = " ";
+for (i = 0; i < p1.length || i < p2.length; i++) {
+    // conparole[i] = " ";
+    if (p1.indexOf([i])) {
+         
+    }
+    
+} console.log(+conparole)
+
+// 4 Scrivere un programma che prenda una stringa come input e restituisca la stessa stringa con i caratteri in ordine inverso.
+
+p1 = "coraggio";
+let inverso= [];
+let count = 0;
+
+for (let i = p1.length - 1; i >=0; i--) {
+    inverso[count] = p1[i];
+    count++
+}
+console.log(inverso)
