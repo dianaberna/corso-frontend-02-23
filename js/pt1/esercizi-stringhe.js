a4c153a4f8e23a3a543c52d3091217759327d811
Diana Bernabei <bernabei.d@gmail.com>
2023-03-09 18:05:02 +0100

    aggiornata teoria js

--- js/pt1/esercizi-stringhe.js
+++ js/pt1/esercizi-stringhe.js
@@ -5,5 +5,16 @@
 // Scrivere un programma che conta quante lettere “e” ci sono nella parola precipitevolissimevolmente 
 // Scrivere un programma che verifica  se due stringhe sono palindrome 
 // Scrivere un programma che verifica se esiste una stringa all'interno di un altra stringa es. cat in categoria, scafo in piroscafo, mente in palesemente → esempi corretti es. mare in categoria → la parola mare non esiste in categoria
+
+let stringa = "palesemente"
+let sottostringa = "mente"
+let indice = stringa.indexOf(sottostringa);
+
+if (indice !== -1){
+  console.log("la sottostringa è contenuta nella stringa")
+}else{
+    console.log("la sottostringa NON è contenuta nella stringa")
+}
+
 // Scrivere un programma che prenda una stringa come input e restituisca la stessa stringa con i caratteri in ordine inverso.
 // Scrivere un programma che prenda due stringhe come input e restituisca una nuova stringa che è la concatenazione delle due stringhe, separate da uno spazio. (simile all’esercizio 12)
--- js/pt1/stringhe.js
+++ js/pt1/stringhe.js
@@ -43,8 +43,65 @@ console.log(nuovaStringa)
 // matrice/array multidimensionali/array bidimensionali --> riga 1 con elementi nella colonna 1 = 1 nella colonna 2 = 2
 // nella riga 2 ho gli elementi nella colonna 1 = 3 nella colonna 2 = 4
 
-// Scrivere un programma che conta quante lettere “e” ci sono nella parola precipitevolissimevolmente 
-// Scrivere un programma che verifica  se due stringhe sono palindrome 
-// Scrivere un programma che verifica se esiste una stringa all'interno di un altra stringa es. cat in categoria, scafo in piroscafo, mente in palesemente → esempi corretti es. mare in categoria → la parola mare non esiste in categoria
-// Scrivere un programma che prenda una stringa come input e restituisca la stessa stringa con i caratteri in ordine inverso.
-// Scrivere un programma che prenda due stringhe come input e restituisca una nuova stringa che è la concatenazione delle due stringhe, separate da uno spazio. (simile all’esercizio 12)
+console.log("--- funzioni stringhe ---")
+
+let animali = ["gatto", "cane", "coniglio"]
+console.log(animali)
+
+console.log(animali.indexOf("cane"))
+
+animali.sort()
+console.log(animali)
+
+animali.push("criceto") // inserisce il nuovo elemento in coda / alla fine
+console.log(animali)
+
+animali.pop() // elimina l'ultimo elemento presente nell'array
+console.log(animali)
+
+animali.reverse()
+console.log(animali)
+
+console.log(animali.slice(0 , 2))  
+// (0, 2) --> 0 =  è la posizione nell'array, 2 = quanti elementi selezionare
+console.log(animali)
+
+animali[1] = "pappagallo"
+console.log(animali)
+
+animali.splice(1, 0, "criceto") // criceto viene aggiunto in posizione 1 spostando in posizione 2 pappagallo
+// (1, 0, "criceto") --> 1 = posizione nell'array dove inserire il nuovo elemento, 0 = quanti elementi eliminare, "criceto" = il nuovo elemento
+console.log(animali)
+
+let animaliInStringa = animali.toString()
+console.log(animaliInStringa)
+console.log(animali)
+let animaliInStringa2 = String(animali)
+console.log(animaliInStringa2)
+console.log(animali)
+
+let salvo = animali.join(" ")
+console.log(salvo)
+
+
+let array1 = ['a', 'b', 'c'];
+let array2 = ['d', 'e', 'f'];
+let array3 = array1.concat(array2);
+
+console.log(array3);
+
+array1 = [1, 2, 3];
+array2 = [4, 5, 6];
+array3Nuovo = []
+
+for (let i = 0; i < array1.length; i++) {
+    array3Nuovo.push(array1[i]);
+}
+for (let i = 0; i < array2.length; i++) {
+    array3Nuovo.push(array2[i]);
+}
+
+array3 = array1.concat(array2);
+
+console.log(array3);
+console.log(array3Nuovo);
