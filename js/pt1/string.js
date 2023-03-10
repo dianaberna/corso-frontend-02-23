1a28a2514cfb4b49318ea720405fbd5e8f5f9e50
Sharon Mae <duransharonmae@gmail.com>
2023-03-10 16:43:50 +0100

    feat: add new file

--- js/pt1/string.js
+++ js/pt1/string.js
@@ -43,33 +43,6 @@ function concatenaStringhe(string1, string2) {
 }
 
 // 6)
-function cifrarioCesare(stringa, numero) {
-  let nuovaStringa = "";
-
-  for (let i = 0; i < stringa.length; i++) {
-    // Converte la lettera corrente in codice ASCII
-    let codiceCarattere = stringa.charCodeAt(i);
-
-    // Controlla se il carattere corrente è una lettera dell'alfabeto
-    if (codiceCarattere >= 65 && codiceCarattere <= 90) {
-      // Calcola il nuovo codice ASCII spostando il carattere di "numero" posizioni
-      codiceCarattere = ((codiceCarattere - 65 + numero) % 26) + 65;
-    } else if (codiceCarattere >= 97 && codiceCarattere <= 122) {
-      codiceCarattere = ((codiceCarattere - 97 + numero) % 26) + 97;
-    }
-
-    // Converte il nuovo codice ASCII in lettera e lo aggiunge alla nuova stringa
-    nuovaStringa += String.fromCharCode(codiceCarattere);
-  }
-
-  return nuovaStringa;
-}
-
-// Esempio di utilizzo
-let stringaDaCifrare = "Ciao, come va?";
-let numeroSpostamento = 5;
-let stringaCifrata = cifrarioCesare(stringaDaCifrare, numeroSpostamento);
-console.log(stringaCifrata); // output: "Hntf, htaj af?"
 
 // 8
 function commonCharacters(str1, str2) {
@@ -82,3 +55,5 @@ function commonCharacters(str1, str2) {
   return result;
 }
 console.log(commonCharacters("Sofia", "Giulia", "Alice", "Ginevra", "Emma"));;
+
+// 9
