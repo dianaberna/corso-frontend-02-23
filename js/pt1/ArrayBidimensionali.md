966e59a56135b22d1a763cd1c61eb038ef6cab15
Sharon Mae <duransharonmae@gmail.com>
2023-03-10 16:37:08 +0100

    feat: add new file

---
+++ js/pt1/ArrayBidimensionali.md
@@ -0,0 +1,7 @@
+1. Scrivere un programma che prenda in input un array bidimensionale di numeri e restituisca la somma di tutti i numeri presenti nell'array
+
+2. Scrivere un programma che prenda in input un array bidimensionale di stringhe e restituisca una stringa contenente tutte le parole presenti nell'array separate da uno spazio.
+
+3. Scrivere un programma  che prenda in input un array bidimensionale di numeri e restituisca un nuovo array contenente la somma di ogni riga dell'array originale.
+
+4. Scrivi un programma che prenda in input un array bidimensionale di numeri e restituisca il numero più grande presente nell'array.
--- js/pt1/string.js
+++ js/pt1/string.js
@@ -1,4 +1,4 @@
-// 1) Scrivere un programma che conta quante lettere “e” ci sono nella parola precipitevolissimevolmente 
+// 1) 
 let parola = 'precipitevolissimevolmente';
 let count = 0;
 
@@ -32,3 +32,53 @@ for (let i = string.length - 1; i >= 0; i--) {
 console.log(`La parola invertita è: ${parolaInvertita}`);
 
 // 5)
+let string1 = 'Hello';
+let string2 = 'World';
+let newString = concatenaStringhe(string1, string2);
+console.log(newString); // output: "Ciao mondo"
+
+function concatenaStringhe(string1, string2) {
+  let newString = string1 + ' ' + string2;
+  return newString;
+}
+
+// 6)
+function cifrarioCesare(stringa, numero) {
+  let nuovaStringa = "";
+
+  for (let i = 0; i < stringa.length; i++) {
+    // Converte la lettera corrente in codice ASCII
+    let codiceCarattere = stringa.charCodeAt(i);
+
+    // Controlla se il carattere corrente è una lettera dell'alfabeto
+    if (codiceCarattere >= 65 && codiceCarattere <= 90) {
+      // Calcola il nuovo codice ASCII spostando il carattere di "numero" posizioni
+      codiceCarattere = ((codiceCarattere - 65 + numero) % 26) + 65;
+    } else if (codiceCarattere >= 97 && codiceCarattere <= 122) {
+      codiceCarattere = ((codiceCarattere - 97 + numero) % 26) + 97;
+    }
+
+    // Converte il nuovo codice ASCII in lettera e lo aggiunge alla nuova stringa
+    nuovaStringa += String.fromCharCode(codiceCarattere);
+  }
+
+  return nuovaStringa;
+}
+
+// Esempio di utilizzo
+let stringaDaCifrare = "Ciao, come va?";
+let numeroSpostamento = 5;
+let stringaCifrata = cifrarioCesare(stringaDaCifrare, numeroSpostamento);
+console.log(stringaCifrata); // output: "Hntf, htaj af?"
+
+// 8
+function commonCharacters(str1, str2) {
+  let result = "";
+  for (let i = 0; i < str1.length; i++) {
+    if (str2.includes(str1[i])) {
+      result += str1[i];
+    }
+  }
+  return result;
+}
+console.log(commonCharacters("Sofia", "Giulia", "Alice", "Ginevra", "Emma"));;
