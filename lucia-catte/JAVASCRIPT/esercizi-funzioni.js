ac5d1c0904a67ce153804d639434bbbe8030bbea
Lucia Catte <cattelucia@gmail.com>
2023-03-13 15:01:18 +0100

    funzioni

---
+++ lucia-catte/JAVASCRIPT/esercizi-funzioni.js
@@ -0,0 +1,16 @@
+
+
+console.log("-- ESERCIZI CON LE FUNZIONI --")
+
+//ES.1: CREA UNA FUNZIONE CHE ACCETTA DUE NUMERI COME ARGOMENTI E NE RESTITUISCE LA MOLTIPLICAZIONE.
+
+console.log("-- ES. 1: --")
+let base = 3;
+let altezza = 5;
+let lato;
+function moltiplicazione (base, altezza){
+    lato  = base * altezza;
+    console.log("prima" + lato)
+    
+} 
+console.log(lato)
