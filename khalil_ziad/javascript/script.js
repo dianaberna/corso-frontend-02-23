dccf52c0eeb5762b9e466e4e489ef25971767973
khalil1444 <zeadessam1000@gmail.com>
2023-03-13 16:12:06 +0100

    ho ogginto la spiegazione per del +32

--- khalil_ziad/javascript/script.js
+++ khalil_ziad/javascript/script.js
@@ -686,7 +686,8 @@ function getTotalPrice(lista) {
 function generaPasswordCasuale(lungezza) 
 {
     for(let i=0;i<lungezza;i++){
-        console.log(String.fromCharCode(Math.floor(Math.random()*100)+32));
+        console.log(String.fromCharCode(Math.floor(Math.random()*100)+32));// ho aggiunto 32 perchè quelli prima sono non visibili
+        
     }
 }
 generaPasswordCasuale(8);
