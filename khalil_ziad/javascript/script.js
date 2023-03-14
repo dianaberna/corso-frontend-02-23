8a5b3ce7387e463efa548b5700c000070efc5960
khalil1444 <zeadessam1000@gmail.com>
2023-03-14 11:23:08 +0100

    esercizio 21 finito

--- khalil_ziad/javascript/script.js
+++ khalil_ziad/javascript/script.js
@@ -733,3 +733,15 @@ let persona=[
 ]
 personaM30(persona);
 
+//ES.21 Convertitore di date
+
+function convertitoreDate(date,tipo=completa){// se tipo non viene specificato visualizzare anno completo
+ 
+    if(tipo=="completa"){
+        return date[3]+date[4]+"/"+date[0]+date[1]+"/"+date[6]+date[7]+date[8]+date[9];
+    } else{
+        return date[3]+date[4]+"/"+date[0]+date[1]+"/"+date[8]+date[9];
+    }
+    
+}
+console.log(convertitoreDate("12/19/2000","abreviata"));
