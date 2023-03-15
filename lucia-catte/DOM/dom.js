a0c786e84c224bf63daf77b1501c1a86f62a8b2a
Lucia Catte <cattelucia@gmail.com>
2023-03-15 16:01:22 +0100

    dom

--- lucia-catte/DOM/dom.js
+++ lucia-catte/DOM/dom.js
@@ -2,60 +2,50 @@ console.log("sono nel file js dom");
 
 //creo il nav
 let nav = document.createElement("nav");
-
 //appendo il nav al body
 document.body.appendChild(nav);
 
 // creo lo span mypage
 let span = document.createElement("span")
+// appendo lo span alla nav
+nav.appendChild(span); 
 
 //creo il testo mypage in span
 let spanText = document.createTextNode("MyPage");
-
 //appendo il testo span allo span
 span.appendChild(spanText);
 
-// appendo il link alla nav
-nav.appendChild(span);
+
 
 //creo il contenitore dell'elenco
 let ul = document.createElement("ul");
-
 //appendo l'ul al nav
 nav.appendChild(ul);
 
-//creo l'elenco a mano
-/*let li = document.createElement("li");
-let li1 = document.createElement("li");
-let li2 = document.createElement("li");
-
-//appendo i li all' ul
-ul.appendChild(li);
-ul.appendChild(li1);
-ul.appendChild(li2);
-*/
-
-
-//creo l'elenco con il con for
-let arrayLi = ["Home", "Why", "Partner", "How"];
-let li = "";
-for (let i = 0; i < arrayLi.length; i++){
-        li = document.createElement("li")
+//creo l'elenco sotto forma di array
+let arrayLi = [
+    {title: "Home", link: "#Home"}, 
+    {title: "Why", link: "#Why"}, 
+    {title: "Partner", link: "#Partner"}, 
+    {title: "How", link: "#How"} 
+];
+for (let i = 0; i < arrayLi.length; i++) {
+    // creo i vari li
+    let li = document.createElement("li");
+    // creo i vari a
+    let a = document.createElement("a");
+    // creo i contenuti dei vari li
+    let liText = document.createTextNode(arrayLi[i].title);
+    //a tutti gli a lego l'attributo href
+    a.setAttribute("href", arrayLi[i].link)
+    //appendo i testi agli a
+    a.appendChild(liText);
+    // appendo i testi a ai li
+    li.appendChild(a);
+    // appendo a ul i li
+    ul.appendChild(li);
 }
-console.log(li)
 
-//appendo i li all' ul
-/ul.appendChild(li);
 
-//creo i singoli link all'interno dell'elenco
-/*let a = document.createElement("a");
-
-//appendo l' a agli il
-li.appendChild(a);
-
-//creo i testi dell'elenco
-let aText = document.createTextNode("Home", "Why", "Partner", "How");
-
-a.appendChild(aText);*/
 
 
