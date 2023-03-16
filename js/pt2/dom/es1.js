0d85f6726a07e955456b09db77850f64dc023a70
Sharon Mae <duransharonmae@gmail.com>
2023-03-16 10:53:39 +0100

    feat: add all

---
+++ js/pt2/dom/es1.js
@@ -0,0 +1,21 @@
+// Creiamo una funzione che generi un colore casuale
+function generateColore() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    
+    var colore = "rgb(" + r + ", " + g + ", " + b + ")";
+    return colore;
+  }
+  
+  // Selezioniamo il body della pagina
+  var body = document.querySelector("body");
+  
+  // Generiamo un colore casuale e lo applichiamo al body come sfondo
+  var background = generateColore();
+  body.style.backgroundColor = background;
+  
+  // Selezioniamo il div con id "color" e stampiamo il colore generato all'interno
+  var divColor = document.querySelector("#color");
+  divColor.textContent = background;
+  
