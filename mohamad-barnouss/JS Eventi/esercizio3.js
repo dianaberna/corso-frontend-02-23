a71dac460a089b2bd671e887fab79726d5db5f3a
Mohamad Barnouss <mohamadbarnouss@gmail.com>
2023-03-17 17:08:58 +0100

    Esercizio 3 eventi Javascript

---
+++ mohamad-barnouss/JS Eventi/esercizio3.js
@@ -0,0 +1,73 @@
+window.addEventListener("load", function () {
+  console.log("prova 3");
+
+  //CREAZIONE BARRA INPUT E BOTTONE
+  let inputBar = document.createElement("input");
+  inputBar.setAttribute("id", "barra"); //ID BARRA INPUT
+  document.body.appendChild(inputBar);
+  let buttonText = document.createTextNode("AGGIUNGI");
+  let buttonType = document.createElement("button");
+  buttonType.setAttribute("id", "bottoneId");
+  buttonType.appendChild(buttonText);
+  document.body.appendChild(buttonType);
+
+  //CREO "ul" e "li"
+  let ulList = document.createElement("ul"); //creo la "ul"
+  // let listLi = document.createElement("li"); // creo "li"
+  //ulList.appendChild(listLi);// provo a farlo dentro eventListener
+  // creo il "div"
+  let divElement = document.createElement("div");
+  divElement.setAttribute("id", "divElement");
+
+  //appendo la "ul" al "div" e di conseguenza il div al body
+  divElement.appendChild(ulList);
+  document.body.appendChild(divElement);
+
+  //Evento bottone click
+  buttonType.addEventListener("click", function () {
+    console.log("Click bottone FUNZIONA");
+    let aggiunta = document.createTextNode(
+      document.getElementById("barra").value
+    );
+    let listLi = document.createElement("li"); //creo list
+    listLi.appendChild(aggiunta);
+    ulList.appendChild(listLi);
+    listLi.setAttribute("class", "listClass");
+
+    //creo bottone X
+    let xtesto = document.createTextNode("X");
+    let xbtn = document.createElement("button");
+    xbtn.appendChild(xtesto);
+    listLi.appendChild(xbtn);
+    xbtn.setAttribute("class", "xbutton");
+
+    //cancella
+    xbtn.addEventListener("click", function () {
+      listLi.setAttribute("class", "delate");
+      ulCestino.appendChild(listLi);
+      console.log(listLi);
+      //cestinoLi.appendChild(listLi);
+      //ulCestino.appendChild(cestinoLi);
+      //listLi.remove(); //si usa cosi prendendo l'elemento list che avevo creato RIVEDERE!!!!!
+    });
+  });
+
+  //cestino
+  let cestinoElement = document.createElement("div");
+  let ulCestino = document.createElement("ul");
+  cestinoElement.appendChild(ulCestino);
+  document.body.appendChild(cestinoElement);
+  let cestinoTesto = document.createTextNode("cestino");
+  let bottoneCestino = document.createElement("button");
+  bottoneCestino.appendChild(cestinoTesto);
+  cestinoElement.appendChild(bottoneCestino);
+
+  bottoneCestino.addEventListener("click", function () {
+    console.log(ulCestino);
+    let elementiDaCancellare = document.getElementsByClassName("delate");
+    for (let i = elementiDaCancellare.length - 1; i >= 0; i--) {
+      //PARTO DALLA FINE E CANCELLO TUTTI GLI ELEMENTI
+      elementiDaCancellare[i].remove();
+    }
+  });
+});
