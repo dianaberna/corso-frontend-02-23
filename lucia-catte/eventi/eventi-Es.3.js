4effdb4fe8181af297a331210f1f6891304aeb53
Lucia Catte <cattelucia@gmail.com>
2023-03-17 17:25:02 +0100

    inizio es 3

--- lucia-catte/eventi/eventi-Es.3.js
+++ lucia-catte/eventi/eventi-Es.3.js
@@ -5,62 +5,45 @@ lasciarli in una seconda lista (una sorta di cestino dei task) da poter svuotare
 click di un altro pulsante (svuota cestino).*/
 
 
-
 window.addEventListener("load", function () {
-	console.log("il body è stato caricato"); 
-
-    let elencoGenitore = document.createElement("ul");
-    document.body.appendChild(elencoGenitore); 
+	console.log(" 3 esercizio "); 
     
-    let primoLi = document.createElement("li");
-    elencoGenitore.appendChild(primoLi);
-    let secondoLi = document.createElement("li");
-    elencoGenitore.appendChild(secondoLi);
-    let terzoLi = document.createElement("li");
-    elencoGenitore.appendChild(terzoLi);
-})
+    let labelBar = document.createElement("input");//creo label input
+    document.body.appendChild(labelBar);// appendo al body
 
+    let button = document.createElement("button");// creo button elimina
+    button.setAttribute("id", "invio")//imposto l'id "invio" al primo button
+    document.body.appendChild(button);// appendo al body
 
+    let invioTxt = document.createTextNode("invio");//imposto il primo button con il testo invio
+    button.appendChild(invioTxt);//appendo il testo al button
 
+    let elencoGenitore = document.createElement("ul");//creo l'elenco ul
+    document.body.appendChild(elencoGenitore);//appendo l'elenco al body
 
+    //evento click "invio testo"
+    invioTxt.addEventListener("click", function (){
+        console.log("evento click di invio");
+        
+        
+        let inseriscoText = document.createTextNode(document.getElementsByTagName("input").value);//creo una variabile che conterrà i valori del label input
+        let primoElenco = document.createElement("li");//creo il primo elenco dove andranno ad inserirsi il valori raccolti dal label input
+        
+        primoElenco.setAttribute("id", "listaInvio")//imposto un id al primo elenco
+        primoElenco.appendChild(inseriscoText);//appendo la variabile contenente i label input al primo elenco puntato
+        elencoGenitore.appendChild(primoElenco);//appendo il sotto-elenco li all'elenco genitore ul
 
+        let eliminaButton = document.createElement("button"); //creo il bottone per eliminare
+        eliminaButton.setAttribute("id", "cancButton");//assegno un id al mio secondo button
+        let eliminaTxt = document.createTextNode("elimina"); //creo il testo che conterrà il button 
 
+        eliminaButton.appendChild(eliminaTxt);//appendo il testo del button al button stesso
+        primoElenco.appendChild(eliminaButton);//appendo il button al mio sotto-elenco
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*window.addEventListener("load", function () {
-let label = document.getElementById("input"); 
-   console.log(label)
-
-let div = this.document.getElementsByTagName("div")[0];
-div.style = "margin: 2rem; width: 400px; height: 400px; background-color: yellow";
-input.value = "margin: 2rem; width: 400px; height: 400px; background-color: yellow"
-
-let pulsante = document.getElementById("clicca");
-    pulsante.addEventListener("click", function(){
-        console.log(label.value)
-    
-    div.style = input.value
+        
+        
     })
-}) */
+
+
+}) 
 
