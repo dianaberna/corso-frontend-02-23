62cba93dc3363b05ae530620739df32111ca173f
Mohamad Barnouss <mohamadbarnouss@gmail.com>
2023-03-17 10:33:40 +0100

    JS eventi Esercizio 1 definitivo

--- mohamad-barnouss/JS Eventi/esercizio1.js
+++ mohamad-barnouss/JS Eventi/esercizio1.js
@@ -14,14 +14,25 @@ window.addEventListener("load", function () {
   document.body.style.background = `rgb(${a},${b},${c})`;
 
   //INSERISCO IL TESTO CENTRALE
-  let paragrafo = document.createTextNode(`rgb(${a},${b},${c})`);
-  let titoloH3 = this.document.createElement("h3");
+  let textRandom = document.createTextNode(`rgb(${a},${b},${c})`);
+  let titoloH3 = this.document.createElement("div");
   titoloH3.setAttribute("id", "mainTitle");
+  //paragrafo dentro il div
+  let paragrafo = document.createElement("p");
+  paragrafo.setAttribute("id", "ptext");
+  paragrafo.appendChild(textRandom);
   titoloH3.appendChild(paragrafo);
   document.body.appendChild(titoloH3); // serve il document prima del body per appenderci qualcosa
 
   document.getElementById("mainTitle").style.background = "white";
-  document.getElementById("mainTitle").style.margin = "45%";
+  document.getElementById("mainTitle").style.transform = "translateY(50vh)";
   document.getElementById("mainTitle").style.width = "10rem";
   document.getElementById("mainTitle").style.height = "2rem";
+  document.getElementById("mainTitle").style.display = "flex";
+  document.getElementById("mainTitle").style.alignItems = "center";
+  document.getElementById("mainTitle").style.justifyContent = "center";
+  document.body.style.display = "flex";
+  document.body.style.alignItems = "center";
+  document.body.style.justifyContent = "center";
+  this.document.style.traslate;
 });
