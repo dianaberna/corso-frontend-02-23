641e4b9e223e70a2223e63ed5667cc737dc20389
Francesc Canzaniello <canzaniello.francesco1988@gmail.com>
2023-03-18 20:30:14 +0100

    esercizio 3 ver 3 completato

--- francesco-canzaniello/js-pt2(personale)/esercizi_eventi/esercizio-eventListener.js
+++ francesco-canzaniello/js-pt2(personale)/esercizi_eventi/esercizio-eventListener.js
@@ -68,65 +68,104 @@
 
 // es 3
 
-window.addEventListener("load", function () {
-    let input = document.createElement("input");
-    document.body.appendChild(input);
+// window.addEventListener("load", function () {
+//     let input = document.createElement("input");
+//     document.body.appendChild(input);
 
-    let pulsante = document.createElement("button");
-    document.body.appendChild(pulsante);
+//     let pulsante = document.createElement("button");
+//     document.body.appendChild(pulsante);
 
-    let testo_btn = document.createTextNode("Aggiungi");
-    pulsante.appendChild(testo_btn);
+//     let testo_btn = document.createTextNode("Aggiungi");
+//     pulsante.appendChild(testo_btn);
 
-    let cestino = document.createElement("button");
-    document.body.appendChild(cestino);
-    let testo_cest = document.createTextNode("Cestino");
-    cestino.appendChild(testo_cest);
+//     let cestino = document.createElement("button");
+//     document.body.appendChild(cestino);
+//     let testo_cest = document.createTextNode("Cestino");
+//     cestino.appendChild(testo_cest);
 
-    // div dei task
-    let div = document.createElement("div");
-    document.body.appendChild(div);
-    div.style.width = "fit-content";
-    div.style.margin = "0px";
+//     // div dei task
+//     let div = document.createElement("div");
+//     document.body.appendChild(div);
+//     div.style.width = "fit-content";
+//     div.style.margin = "0px";
 
-    // lista task
-    let ul = document.createElement("ul");
-    div.appendChild(ul);
-    ul.style.margin = "0px";
+//     // lista task
+//     let ul = document.createElement("ul");
+//     div.appendChild(ul);
+//     ul.style.margin = "0px";
 
-    // cestino
-    let divCestino = document.createElement("div");
-    document.body.appendChild(divCestino);
-    let ulCestino = document.createElement("ul");
-    divCestino.appendChild(ulCestino);
-    ulCestino.style.margin = "0px";
+//     // cestino
+//     let divCestino = document.createElement("div");
+//     document.body.appendChild(divCestino);
+//     let ulCestino = document.createElement("ul");
+//     divCestino.appendChild(ulCestino);
+//     ulCestino.style.margin = "0px";
 
-    divCestino.style.width = "fit-content";
-    divCestino.style.margin = "0px";
+//     divCestino.style.width = "fit-content";
+//     divCestino.style.margin = "0px";
+    
+//     // aggunge un elemento 
+//     pulsante.addEventListener("click", function () {
+//         let li = document.createElement("li");
+//         ul.appendChild(li);
+//         let testo_li = document.createTextNode(input.value);
+//         li.appendChild(testo_li);
 
-    pulsante.addEventListener("click", function () {
-        let li = document.createElement("li");
-        ul.appendChild(li);
-        let testo_li = document.createTextNode(input.value);
-        li.appendChild(testo_li);
-        let pulsanteX = document.createElement("button");
-        li.appendChild(pulsanteX);
-        let testo_X = document.createTextNode("X");
-        pulsanteX.appendChild(testo_X);
-        pulsanteX.style.marginLeft = "5px";
+//         let pulsanteX = document.createElement("button");
+//         li.appendChild(pulsanteX);
+//         let testo_X = document.createTextNode("X");
+//         pulsanteX.appendChild(testo_X);
+//         pulsanteX.style.marginLeft = "5px";
 
-        pulsanteX.addEventListener("click", function () {
-            let liCestino = li;
-            ulCestino.appendChild(liCestino);
-            console.log(ulCestino + " cestino");
-            liCestino.setAttribute("class", "delete");
-        });
-    });
-    cestino.addEventListener("click", function () {
-        let cestinoDaSvuotare = document.getElementsByClassName("delete");
-        console.log(cestinoDaSvuotare.length + " lunghezza lista");
-        for (let i = cestinoDaSvuotare.length - 1; i >= 0; i--) {
-            cestinoDaSvuotare[i].remove();
-        }
-    });
-});
+//         let liCestino = li;
+        
+        
+//         //svuota il cestino
+//         cestino.addEventListener("click", function () {
+//             let cestinoDaSvuotare = document.getElementsByClassName("delete");                    
+            
+//             for (let i = cestinoDaSvuotare.length - 1; i >= 0; i--) {
+//                 cestinoDaSvuotare[i].remove();
+//             }            
+//             testo_cest.textContent = `Cestino [${document.getElementsByClassName("delete").length}]`
+            
+//         });
+
+
+
+//         //butta un elemento nel cestino
+//         pulsanteX.addEventListener("click", function () {
+            
+//             ulCestino.appendChild(liCestino);
+//             let pulsanteRipr = document.createElement("button");
+//             liCestino.appendChild(pulsanteRipr);
+//             let testo_ripr = document.createTextNode("Ripristina");
+//             pulsanteX.remove()
+//             pulsanteRipr.appendChild(testo_ripr);
+//             pulsanteRipr.style.marginLeft = "5px";
+                
+//             liCestino.setAttribute("class", "delete");
+            
+            
+//             testo_cest.textContent = `Cestino [${document.getElementsByClassName("delete").length}]`
+            
+//             // rispristina un elemento della task
+//             pulsanteRipr.addEventListener("click", function(){
+//                 liCestino.removeAttribute("class")
+//                 ul.appendChild(liCestino)              
+//                 li.appendChild(pulsanteX);                
+//                 pulsanteRipr.remove()
+//                 testo_cest.textContent = `Cestino [${document.getElementsByClassName("delete").length}]`
+//             });
+//         }); 
+        
+        
+        
+
+//     });
+    
+// });
+
+
+
+//  es 4
