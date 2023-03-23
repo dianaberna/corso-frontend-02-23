85aada55eec3506f261109bded66d21bdaff0f3b
Diana Bernabei <bernabei.d@gmail.com>
2023-03-23 14:33:33 +0100

    aggiornato js

---
+++ js/pt3/modularità/script5.js
@@ -0,0 +1,11 @@
+console.log("file5")
+
+function funzione1file3(){
+    console.log("sono in funzione 1 file3")
+}
+
+function funzione2file3(){
+    console.log("sono in funzione 2 file3")
+}
+
+export {funzione1file3, funzione2file3}
